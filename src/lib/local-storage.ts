b65936c3d97bb7ef538bcbf683b0799d68893036
adgentai <adgenttech@gmail.com>
2025-06-19 09:06:21 +0100

    fix: storage

--- src/lib/local-storage.ts
+++ src/lib/local-storage.ts
@@ -4,23 +4,14 @@ import Taro from '@tarojs/taro'
 
 const localStorageAdapter: SupportedStorage = {
   getItem: (key) => {
-    if (!isBrowser()) {
-      return null
-    }
 
     return Taro.getStorageSync(key)
   },
   setItem: (key, value) => {
-    if (!isBrowser()) {
-      return
-    }
 
     Taro.setStorageSync(key, value)
   },
   removeItem: (key) => {
-    if (!isBrowser()) {
-      return
-    }
 
     Taro.removeStorageSync(key)
   },
